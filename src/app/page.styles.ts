5e9782b4b9d9312d919602ccd889bcb6b6667656
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-29 22:20:52 +0100

    feat: work on index page design

---
+++ src/app/page.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+import { media } from "@/ui/theme/breakpoints";
+
+export const Root = styled.div`
+  padding: ${({ theme }) => theme.spacing(8, 4)};
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+
+  gap: ${({ theme }) => theme.spacing(4)};
+  margin-top: ${({ theme }) => theme.spacing(4)};
+
+  ${media.tablet} {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+  ${media.laptop} {
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  }
+`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,22 @@ import { useQuery } from "@tanstack/react-query";
 import services from "@/lib/api/services";
 import querykeys from "@/lib/querykeys";
 
+import VideoPreview from "./components/video-preview-card";
+import { Grid, Root } from "./page.styles";
+
 export default function IndexPage() {
   const { data: videos } = useQuery({
     queryKey: querykeys.youtube.search({}),
     queryFn: () => services.youtube.search({}),
   });
 
-  console.log(videos);
-
-  return <>Index page</>;
+  return (
+    <Root>
+      <Grid>
+        {videos?.map((video) => (
+          <VideoPreview key={video.id.videoId} preview={video} />
+        ))}
+      </Grid>
+    </Root>
+  );
 }
