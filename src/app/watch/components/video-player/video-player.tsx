91fe31b8e1d2bd7567b872527322858b6d55dd65
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-02-29 23:41:11 +0100

    feat(video-player): add

---
+++ src/app/watch/components/video-player/video-player.tsx
@@ -0,0 +1,11 @@
+import { Video } from "@/lib/models/video";
+
+import { Iframe } from "./video-player.styles";
+
+export type VideoPlayerProps = {
+  video: Video;
+};
+
+export default function VideoPlayer({ video }: VideoPlayerProps) {
+  return <Iframe src={`https://www.youtube.com/embed/${video.videoId}`} allowFullScreen />;
+}
