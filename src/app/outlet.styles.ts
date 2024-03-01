1f176c3b8e7b8e58b8c13a88b7cdf491cd46c4c0
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-03-01 09:49:39 +0100

    styles: make header sticky

--- src/app/outlet.styles.ts
+++ src/app/outlet.styles.ts
@@ -9,7 +9,6 @@ export const Main = styled.main`
   max-height: 100vh;
   overflow: auto;
   flex: 1;
-  padding: ${({ theme }) => theme.spacing(8, 4)};
   max-width: 1800px;
   margin: 0 auto;
 `;
@@ -17,4 +16,12 @@ export const Main = styled.main`
 export const Content = styled.div`
   padding: ${({ theme }) => theme.spacing(8, 4)};
   height: 100%;
+`;
+
+export const Header = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+  padding: ${({ theme }) => theme.spacing(2, 4)};
+  background-color: ${({ theme }) => theme.colors.white};
 `;
--- src/app/outlet.tsx
+++ src/app/outlet.tsx
@@ -3,7 +3,7 @@ import { Outlet, useNavigate } from "react-router";
 import ScrollToTop from "./components/scroll-to-top/scroll-to-top";
 import SearchInput from "./components/search-input/search-input";
 import Sidebar from "./components/sidebar";
-import { Content, Main, Root } from "./outlet.styles";
+import { Content, Header, Main, Root } from "./outlet.styles";
 
 export default function IndexOutlet() {
   const navigate = useNavigate();
@@ -12,7 +12,9 @@ export default function IndexOutlet() {
     <Root>
       <Sidebar />
       <Main>
-        <SearchInput onSearch={(q) => navigate({ pathname: "/", search: q ? `?q=${q}` : "" })} />
+        <Header>
+          <SearchInput onSearch={(q) => navigate({ pathname: "/", search: q ? `?q=${q}` : "" })} />
+        </Header>
         <Content>
           <Outlet />
         </Content>
